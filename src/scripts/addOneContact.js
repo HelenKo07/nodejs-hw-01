a85144278721b418504e463161e2751cb03ec518
Alena_Korma <alena.gorova93@gmail.com>
2025-05-02 12:07:16 +0300

    commit message

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,18 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from "../utils/createFakeContact.js";
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+export const addOneContact = async () => {
+    try {
+        const contacts = await readContacts();
+        const addOneContact = createFakeContact();
+        const updateContacts = [...contacts, addOneContact];
+        await writeContacts(updateContacts);
+        console.log('New contact successfully added.');
+
+    } catch (error) {
+        console.error('Error to add new contact:', error.message);
+    }
+};
 
 addOneContact();
